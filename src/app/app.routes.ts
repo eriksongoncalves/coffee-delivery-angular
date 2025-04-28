5f028ccd06875a4f23a0bab1bc756f553c7192da
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-04-28 14:00:08 -0300

    feat: initial configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { OrderComponent } from './pages/order/order.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  }
+]
