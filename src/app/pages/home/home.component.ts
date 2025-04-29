f7c2157d8a2446de5fb065df78a55e054ac19ca1
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-04-29 11:31:54 -0300

    feat: home intro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core'
 
+import { ShoppingCartFillIconComponent } from '../../components/icons/shopping-cart-fill-icon.component'
+import { PackageIconComponent } from '../../components/icons/package-icon.component'
+import { CoffeeFillIconComponent } from '../../components/icons/coffee-fill-icon.component'
+import { TimerFillIconComponent } from '../../components/icons/timer-fill-icon.component'
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ShoppingCartFillIconComponent,
+    PackageIconComponent,
+    CoffeeFillIconComponent,
+    TimerFillIconComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
